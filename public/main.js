da7fd8e0f17b8a6c7754f87916eddce4c7689fab
sdserage <xttocsurge51@gmail.com>
2018-12-27 18:20:35 -0700

    public/main.js

--- public/main.js
+++ public/main.js
@@ -6,16 +6,26 @@ const isDev = require('electron-is-dev');
 
 let mainWindow;
 let imageWindow;
+let settingsWindow;
 
 function createWindow() {
   mainWindow = new BrowserWindow({width: 900, height: 680, webPreferences: {webSecurity: false}});
-  imageWindow = new BrowserWindow({width: 600, height: 600, parent: mainWindow, show: false, webPreferences: {webSecurity: false}});
+  imageWindow = new BrowserWindow({width: 600, height: 600, parent: mainWindow, show: false});
+  settingsWindow = new BrowserWindow({width: 600, height: 600, parent: mainWindow, show: false});
+
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   imageWindow.loadURL(isDev ? 'http://localhost:3000/image' : `file://${path.join(__dirname, '../build/index.html')}`);
+  settingsWindow.loadURL(isDev ? 'http://localhost:3000/settings' : `file://${path.join(__dirname, '../build/index.html')}`);
+
   mainWindow.on('closed', () => mainWindow = null);
+
   imageWindow.on('close', e => {
     e.preventDefault();
     imageWindow.hide();
+  });
+  settingsWindow.on('close', e => {
+    e.preventDefault();
+    settingsWindow.hide();
   });
 }
 
@@ -36,4 +46,8 @@ app.on('activate', () => {
 ipcMain.on('toggle-image', (event, arg) => {
   imageWindow.show();
   imageWindow.webContents.send('image', arg);
+});
+
+ipcMain.on('toggle-settings', () => {
+  settingsWindow.isVisible() ? settingsWindow.hide() : settingsWindow.show();
 });
